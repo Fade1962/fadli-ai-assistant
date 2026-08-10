4d1d63263de2bff91adcb7cff608367a25e05449
Fade1962 <123169101+Fade1962@users.noreply.github.com>
2026-08-10 16:49:04 +0800

    router.py

--- file_processor/router.py
+++ file_processor/router.py
@@ -1,5 +1,3 @@
-import os
-
 from .pdf import process_pdf
 from .image import process_image
 from .document import process_document
@@ -9,54 +7,41 @@ from .presentation import process_presentation
 
 def process_file(filename):
 
-    extension = os.path.splitext(
-        filename
-    )[1].lower()
+    ext = filename.lower().split(".")[-1]
 
 
-    if extension == ".pdf":
-
+    if ext == "pdf":
         return process_pdf(filename)
 
 
-    elif extension in (
-        ".jpg",
-        ".jpeg",
-        ".png",
-        ".webp"
-    ):
-
+    if ext in [
+        "png",
+        "jpg",
+        "jpeg",
+        "webp"
+    ]:
         return process_image(filename)
 
 
-    elif extension in (
-        ".docx",
-        ".doc"
-    ):
-
+    if ext in [
+        "docx"
+    ]:
         return process_document(filename)
 
 
-    elif extension in (
-        ".xlsx",
-        ".xls",
-        ".csv"
-    ):
-
+    if ext in [
+        "xlsx",
+        "csv"
+    ]:
         return process_spreadsheet(filename)
 
 
-    elif extension in (
-        ".pptx",
-        ".ppt"
-    ):
-
+    if ext in [
+        "pptx"
+    ]:
         return process_presentation(filename)
 
 
-    else:
-
-        return (
-            "Format file belum didukung: "
-            + extension
-        )
+    return (
+        "Format file belum didukung."
+    )
